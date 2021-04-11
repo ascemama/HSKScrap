b2a749aae730c3eb25e2b33f5516161e55b4ee71
Antoine Scemama <AScemama@brainloop.com>
2021-04-11 17:19:26 +0800

    add distinct count

--- analyse.py
+++ analyse.py
@@ -3,6 +3,8 @@ class AnalyseString:
     def __init__(self, inputString):
         self.inputString=inputString
         self.inputStringLength=len(inputString)
+        self.inputStringNbOfChineseChar=0
+        self.inputStringNbOfDistinctChineseChar=0
         self.nbHSK1Char=0
         self.nbHSK2Char=0
         self.HSK1Array=AnalyseString.loadHSK('HSK1.txt')
@@ -11,54 +13,112 @@ class AnalyseString:
         self.HSK4Array=AnalyseString.loadHSK('HSK4.txt')
         self.HSK5Array=AnalyseString.loadHSK('HSK5.txt')
         self.HSK6Array=AnalyseString.loadHSK('HSK6.txt')
+        self.HSK1MdArray=AnalyseString.fillHSKMdArray(self.HSK1Array)
+        self.HSK2MdArray=AnalyseString.fillHSKMdArray(self.HSK2Array)
+        self.HSK3MdArray=AnalyseString.fillHSKMdArray(self.HSK3Array)
+        self.HSK4MdArray=AnalyseString.fillHSKMdArray(self.HSK4Array)
+        self.HSK5MdArray=AnalyseString.fillHSKMdArray(self.HSK5Array)
+        self.HSK6MdArray=AnalyseString.fillHSKMdArray(self.HSK6Array)
+        self.NonHSKMdArray={}
+         
          
     def loadHSK(hskFilePath):
         with open(hskFilePath, 'r', encoding='utf8') as f:
             HSK1STR = f.read()
-            print(hskFilePath+" List:\n")
-            print(list(set(HSK1STR)))
+            #print(hskFilePath+" List:\n")
+            #print(list(set(HSK1STR)))
         return list(set(HSK1STR))
 
-    def characterOcurrence(hskArray, source):
+    def fillHSKMdArray(HSKArray):
         HSKMdArray={}
-        for character in hskArray:
+        for character in HSKArray:
             HSKMdArray[character]=0
-        for character in source:
-            if character in hskArray:
-                HSKMdArray[character]=HSKMdArray[character]+1
-        print("HSKMdTArray List:\n")
-        print(HSKMdArray)
         return HSKMdArray
 
+    def characterOcurrence(self,source):
+        for character in source:
+            if u'\u4e00' <= character <= u'\u9fff':
+                self.inputStringNbOfChineseChar=self.inputStringNbOfChineseChar+1
+                if character in self.HSK1MdArray:
+                    self.HSK1MdArray[character]=self.HSK1MdArray[character]+1
+                else:
+                    if character in self.HSK2MdArray:
+                        self.HSK2MdArray[character]=self.HSK2MdArray[character]+1
+                    else:
+                        if character in self.HSK3MdArray:
+                            self.HSK3MdArray[character]=self.HSK3MdArray[character]+1
+                        else:
+                            if character in self.HSK4MdArray:
+                                self.HSK4MdArray[character]=self.HSK4MdArray[character]+1
+                            else:
+                                if character in self.HSK5MdArray:
+                                    self.HSK5MdArray[character]=self.HSK5MdArray[character]+1
+                                else:
+                                    if character in self.HSK6MdArray:
+                                        self.HSK6MdArray[character]=self.HSK6MdArray[character]+1
+                                    else:
+                                        if character not in self.NonHSKMdArray:
+                                            self.NonHSKMdArray[character]=0
+                                        else:
+                                            self.NonHSKMdArray[character]=self.NonHSKMdArray[character]+1
+
+        #print("HSKMdTArray List:\n")
+        #print(HSKMdArray)
+        #return HSKMdArray
+
     def AnalysHSKProfile(self):
-        self.HSK1MdArray=AnalyseString.characterOcurrence(self.HSK1Array,self.inputString)
-        self.HSK2MdArray=AnalyseString.characterOcurrence(self.HSK2Array,self.inputString)
-        self.HSK3MdArray=AnalyseString.characterOcurrence(self.HSK3Array,self.inputString)
-        self.HSK4MdArray=AnalyseString.characterOcurrence(self.HSK4Array,self.inputString)
-        self.HSK5MdArray=AnalyseString.characterOcurrence(self.HSK5Array,self.inputString)
-        self.HSK6MdArray=AnalyseString.characterOcurrence(self.HSK6Array,self.inputString)
+        self.characterOcurrence(self.inputString)
+         
     
     def ComputeNbHSKXChar(self):
         print("NbChar in Site: "+str(self.inputStringLength))
+        print("Nb Chinese Char in Site: "+str(self.inputStringNbOfChineseChar))
         self.NbHSK1Char=AnalyseString.ComputeNbChar(self.HSK1MdArray)
         self.NbHSK2Char=AnalyseString.ComputeNbChar(self.HSK2MdArray)
         self.NbHSK3Char=AnalyseString.ComputeNbChar(self.HSK3MdArray)
         self.NbHSK4Char=AnalyseString.ComputeNbChar(self.HSK4MdArray)
         self.NbHSK5Char=AnalyseString.ComputeNbChar(self.HSK5MdArray)
         self.NbHSK6Char=AnalyseString.ComputeNbChar(self.HSK6MdArray)
+        self.NbNonHSKChar=AnalyseString.ComputeNbChar(self.NonHSKMdArray)
          
-        print("NbChar HSK1: "+str(AnalyseString.ComputeNbChar(self.HSK1MdArray)))
-        print("NbChar HSK2: "+str(AnalyseString.ComputeNbChar(self.HSK2MdArray)))
-        print("NbChar HSK3: "+str(AnalyseString.ComputeNbChar(self.HSK3MdArray)))
-        print("NbChar HSK4: "+str(AnalyseString.ComputeNbChar(self.HSK4MdArray)))
-        print("NbChar HSK5: "+str(AnalyseString.ComputeNbChar(self.HSK5MdArray)))
-        print("NbChar HSK6: "+str(AnalyseString.ComputeNbChar(self.HSK6MdArray)))
-        print("NbChar Non-HSK: "+str(self.inputStringLength-self.NbHSK1Char-self.NbHSK2Char-self.NbHSK3Char-self.NbHSK4Char-self.NbHSK5Char-self.NbHSK6Char))
+        print("NbChar HSK1: "+str(self.NbHSK1Char))
+        print("NbChar HSK2: "+str(self.NbHSK2Char))
+        print("NbChar HSK3: "+str(self.NbHSK3Char))
+        print("NbChar HSK4: "+str(self.NbHSK4Char))
+        print("NbChar HSK5: "+str(self.NbHSK5Char))
+        print("NbChar HSK6: "+str(self.NbHSK6Char))
+        print("NbChar Non-HSK: "+str(self.NbNonHSKChar))
 
     def ComputeNbChar(HSKXMdArray):
         NbChar=0
         for key in HSKXMdArray:
             NbChar=NbChar+HSKXMdArray[key]
+        return NbChar
+    
+    def ComputeNbHSKXDistinctChar(self):
+
+        self.NbHSK1DistinctChar=AnalyseString.ComputeNbDistinctChar(self.HSK1MdArray)
+        self.NbHSK2DistinctChar=AnalyseString.ComputeNbDistinctChar(self.HSK2MdArray)
+        self.NbHSK3DistinctChar=AnalyseString.ComputeNbDistinctChar(self.HSK3MdArray)
+        self.NbHSK4DistinctChar=AnalyseString.ComputeNbDistinctChar(self.HSK4MdArray)
+        self.NbHSK5DistinctChar=AnalyseString.ComputeNbDistinctChar(self.HSK5MdArray)
+        self.NbHSK6DistinctChar=AnalyseString.ComputeNbDistinctChar(self.HSK6MdArray)
+        self.NbNonHSKDistinctChar=AnalyseString.ComputeNbDistinctChar(self.NonHSKMdArray)
+         
+        print("NbChar distinct HSK1: "+str(self.NbHSK1DistinctChar))
+        print("NbChar distinct HSK2: "+str(self.NbHSK2DistinctChar))
+        print("NbChar distinct HSK3: "+str(self.NbHSK3DistinctChar))
+        print("NbChar distinct HSK4: "+str(self.NbHSK4DistinctChar))
+        print("NbChar distinct HSK5: "+str(self.NbHSK5DistinctChar))
+        print("NbChar distinct HSK6: "+str(self.NbHSK6DistinctChar))
+        print("NbChar distinct Non-HSK: "+str(self.NbNonHSKDistinctChar))
+
+
+    def ComputeNbDistinctChar(HSKXMdArray):
+        NbChar=0
+        for key in HSKXMdArray:
+            if HSKXMdArray[key]!= 0:
+                NbChar=NbChar+1
         return NbChar
 
     
@@ -70,6 +130,7 @@ def main():
         Ana=AnalyseString(sitestr)
         Ana.AnalysHSKProfile()
         Ana.ComputeNbHSKXChar()
+        Ana.ComputeNbHSKXDistinctChar()
 
 if __name__ == "__main__":
     main()
